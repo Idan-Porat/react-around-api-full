55755226aea5479879471771058799c814239dc7
PracticumStudent <idanx061@gmail.com>
2022-01-07 10:52:29 +0200

    delete one line of code

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -3,14 +3,14 @@ const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 const { celebrate, Joi } = require('celebrate');
-const validateUrl = require('');
+
 
 cardRouter.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     imageLink: Joi.string().required().custom(validateUrl),
   }).unknown(true)
-}), createPost);
+}), createCard);
 cardRouter.get('/cards', getCards);
 cardRouter.delete('/cards/:cardId', deleteCard);
 cardRouter.put('/cards/:cardId/likes', likeCard);
