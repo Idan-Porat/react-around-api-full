25058db4cdf847f11bc6cb6b97b417cfaf913850
PracticumStudent <idanx061@gmail.com>
2022-01-12 18:06:28 +0200

    update

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -83,19 +83,22 @@ function App() {
     }
   }, [token])
 
-  const handleCardLike = (card) => {
-    // Check one more time if this card was already liked
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const handleCardLike = (card) => { 
 
-    // Send a request to the Api and getting the updated card data
-    Api
-      .changeLikeCardStatus(card._id, !isLiked, token)
-      .then((newCard) => {
-        const { data } = newCard;
-        setCards((state) => state.map((c) => (c._id === card._id ? data : c)));
-      })
-      .catch((err) => console.log(err));
-  }
+    // Check one more time if this card was already liked 
+    const isLiked = card.likes.some(i => i._id === currentUser._id); 
+    // Send a request to the API and getting the updated card data 
+    if (isLiked) { 
+      Api.unLikeCard(card._id, token).then((newCard) => { 
+        setCards((state) => state.map((c) => c._id === card._id ? newCard : c)); 
+
+      }).catch((error) => console.log(error)); 
+    } else { 
+      Api.likeCard(card._id, token).then((newCard) => { 
+        setCards((state) => state.map((c) => c._id === card._id ? newCard : c)); 
+      }).catch((error) => console.log(error)); 
+    } 
+  } 
 
   const handleDeleteCard = async () => {
     const id = selectedCard._id;
--- frontend/src/components/ImagePopup.js
+++ frontend/src/components/ImagePopup.js
@@ -7,7 +7,7 @@ function ImagePopup(props) {
             <div className="popup__container popup__container_theme_open-photo">
                 <button type="button" className="popup__close-button" onClick={onClose} aria-label="close button"></button>
                 <figure>
-                    <img className="popup__open-image" alt={!cardObj ? null : cardObj.name} src={cardObj.link} />
+                    <img className="popup__open-image" alt={!cardObj ? null : cardObj.name} src={cardObj.imageLink} />
                     <figcaption className="popup__open-image-caption">{cardObj.name}</figcaption>
                 </figure>
             </div>
