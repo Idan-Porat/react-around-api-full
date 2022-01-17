d3c00ebdac21cb7f4008c501e40ed1bdad31e35a
PracticumStudent <idanx061@gmail.com>
2022-01-17 13:38:39 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type, Accept, authorization, Authorization',
+    'X-Requested-With,content-type',
   );
   res.setHeader(
     'Access-Control-Allow-Credentials',
