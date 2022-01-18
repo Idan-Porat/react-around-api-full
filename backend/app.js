b7d45fe2e3ac3127530d160b32046f484ce1048f
PracticumStudent <idanx061@gmail.com>
2022-01-18 11:37:49 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const cardRouter = require('./routes/cards');
 const auth = require('./middleware/auth');
 const { createUser, login } = require('./controllers/users');
 const { requestLogger, errorLogger } = require('./middleware/logger');
+const ErrorHandler = require('./middleware/errorHandler');
 
 const { PORT = 3000, BASE_PATH } = process.env;
 const app = express();
@@ -69,21 +70,16 @@ app.use(errorLogger); // enabling the error logger
 app.use(errors());
 
 app.use((req, res, next) => {
-  res.status(404).send({ message: `Route ${req.url} Not found.` });
+  res.send(() => {
+    throw new ErrorHandler(`Route ${req.url} Not found.`, 404);
+  });
   next();
 });
 
 app.use((err, req, res, next) => {
-  // if an error has no status, display 500
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      // check the status and display a message based on it
-      message: statusCode === 500
-        ? 'An error occurred on the server'
-        : message,
-    });
+  res.send(() => {
+    throw new ErrorHandler('An error occurred on the server', 500);
+  });
   next();
 });
 
--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -3,6 +3,9 @@ class ErrorHandler {
     this.message = message;
     this.statusCode = statusCode;
   }
-}
 
+  error() {
+    return `Error status: ${this.statusCode} and the reason is: ${this.message} `;
+  }
+}
 module.exports = ErrorHandler;
