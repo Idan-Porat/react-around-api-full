3ca3147feed255b3251640877c2c782d17905b51
PracticumStudent <idanx061@gmail.com>
2022-01-20 12:42:25 +0200

    update

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,7 +9,9 @@ userRouter.get('/users', getAllUsers);
 userRouter.get('/users/me', getCurrentUser);
 userRouter.get('/users/:userId', celebrate({
   body: Joi.object().keys({
-    _id: Joi.string().hex().required(),
+    user: Joi.object().keys({
+      _id: Joi.string().hex().required(),
+    }).unknown(true),
   }).unknown(true),
   params: Joi.object()
     .keys({
@@ -23,7 +25,7 @@ userRouter.patch('/users/me/avatar', celebrate({
   }).unknown(true),
   params: Joi.object()
     .keys({
-      userId: Joi.string().hex().required(),
+      avatar: Joi.string().required(),
     }).unknown(true),
 }), updateAvatar);
 module.exports = userRouter;
